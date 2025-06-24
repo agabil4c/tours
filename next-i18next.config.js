b54127e2bc50e4ed50b9a866c00ab6c5ed947b67
Bruce Agaba <agabil4c@gmail.com>
2025-06-24 11:00:47 +0300

    sorted

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr'], // Add all your supported languages
+    locales: ['en', 'fr', 'de'], // Add all your supported languages
     //localeDetection: true,
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
